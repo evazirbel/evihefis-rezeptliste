21ee270193508169ea1726ce6ae458e556be3ff0
Eva Zirbel <eva.zirbel@gmx.net>
2025-03-23 18:11:49 +0100

    Add GitHub Actions workflow for website deployment and enhance HTML/CSS structure

--- script.js/recipe_calculator.js
+++ script.js/recipe_calculator.js
@@ -1,7 +1,6 @@
 const rezepte = {
     american_style: { mehl: 128.27, wasser: 79.53, hefe: 0.27, salz: 1.93, oel: 0, zucker:0 },
     ny_style: { mehl: 154.0, wasser: 86.3, hefe: 0.2, salz: 2.7, oel: 10.8, zucker:6.2 },
-    
 };
 
 function berechneZutaten() {
@@ -14,11 +13,12 @@ function berechneZutaten() {
     const hefe = zutaten.hefe * anzahl;
     const salz = zutaten.salz * anzahl;
 
-    const zutatenText = `Für ${anzahl} Pizza(s) (${rezept}) benötigst du: 
-    ${mehl}g Mehl, 
-    ${wasser}ml Wasser, 
-    ${hefe}g Hefe, 
-    ${salz}g Salz.`;
+    const zutatenText = `Für ${anzahl} Pizza(s) benötigst du: 
+
+    ${mehl} g Mehl 
+    ${wasser} ml Wasser 
+    ${hefe} g Hefe 
+    ${salz} g Salz `;
     
     document.getElementById('zutaten').innerText = zutatenText;
 }
